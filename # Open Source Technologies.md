185ffba21dee31782856e25e0959e62a4524451b
https://github.com/tusharikatashu <tusharikatashu@gmail.com>
2026-02-03 22:01:07 +0530

    Initialised with README

---
+++ # Open Source Technologies.md
@@ -0,0 +1,2 @@
+# Open Source Technologies
+This repo is for Open Source Technologies Lab.
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to OST Lab Exercises!" << std::endl;
+    return 0;
+}
